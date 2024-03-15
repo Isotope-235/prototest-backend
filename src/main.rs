782dec3503720d7f1b4f7db1be1dc8022654a80e
Jonathan Jensen <jonathan.obsidion@gmail.com>
2024-03-15 15:45:21 +0100

    Actually sending correct canvas version

--- src/main.rs
+++ src/main.rs
@@ -52,7 +52,7 @@ impl Drawing for TestService {
             while let Some(canvas) = stream.message().await? {
                 let mut lock = arc_clone.lock().await;
                 *lock = canvas::merge(&*lock, &canvas);
-                yield canvas;
+                yield (*lock).clone();
             }
         };
 
