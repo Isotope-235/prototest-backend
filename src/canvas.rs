111ad9242ec29832e38c218546827ca3949ef3f1
surgebound <jonathan.obsidion@gmail.com>
2024-03-23 21:40:54 +0100

    Added multiple rooms and multiple resolutions functionality

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,17 +1,30 @@
 use crate::proto::DrawingCanvas;
 
-const WIDTH: usize = 50;
-const HEIGHT: usize = 50;
-const SIZE: usize = WIDTH * HEIGHT;
-
-pub fn blank() -> DrawingCanvas {
-    let contents = std::iter::repeat(0).take(SIZE).collect();
+pub fn blank(width: usize, height: usize) -> DrawingCanvas {
+    let contents = std::iter::repeat(0).take(width * height).collect();
     DrawingCanvas {
         contents,
+        width: width as i32,
+        height: height as i32
     }
 }
 
-pub fn merge_into(target: &mut DrawingCanvas, source: &DrawingCanvas) {
+pub fn try_merge_into(target: &mut DrawingCanvas, source: &DrawingCanvas) -> Result<(), String>{
+
+    if source.width != target.width {
+        return Err(format!("Source and target dimensions do not match! Source canvas width is {0} but width required by the target canvas is {1}.", source.width, target.width));
+    }
+
+    if source.height != target.height {
+        return Err(format!("Source and target dimensions do not match! Source canvas height is {0} but height required by the target canvas is {1}.", source.height, target.height));
+    }
+
+    let source_len = source.contents.len();
+    let target_size = target.width * target.height;
+    if source_len != target_size.max(0) as usize {
+        return Err(format!("Source and target dimensions do not match! The number of pixels in the source canvas is {source_len}, but the number of pixels required by the target canvas is {target_size} ({0} * {1}.)", target.width, target.height));
+    }
+
     target
         .contents
         .iter_mut()
@@ -22,5 +35,38 @@ pub fn merge_into(target: &mut DrawingCanvas, source: &DrawingCanvas) {
                 0 => *t,
                 o => o,
             }
-        })
+        });
+
+    Ok(())
+}
+
+pub fn check_invariants(canvas: &DrawingCanvas) -> CheckInvariantsResult {
+    use CheckInvariantsResult::*;
+    let DrawingCanvas { contents, width, height } = canvas;
+
+    if *width < 1 {
+        return NegativeWidth;
+    }
+
+    if *height < 1 {
+        return NegativeHeight;
+    }
+
+    let Some(checked_size) = width.checked_mul(*height) else {
+        return OverflowingSize;
+    };
+
+    if checked_size as usize != contents.len() {
+        return MismatchedSize;
+    }
+
+    Fine
+}
+
+pub enum CheckInvariantsResult {
+    Fine,
+    NegativeWidth,
+    NegativeHeight,
+    OverflowingSize,
+    MismatchedSize
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use std::sync::{Arc, Mutex};
 
 use proto::{
-    drawing_server::{Drawing, DrawingServer},
-    DrawingCanvas, HealthCheckRequest, Unit,
+    drawing_server::{Drawing, DrawingServer}, CreateRoomRequest, CreateRoomResponse, DrawingCanvas, HealthCheckRequest, PullCanvasRequest, QueryRoomsRequest, QueryRoomsResponse, RoomDetails, UploadCanvasRequest, UploadCanvasResponse
 };
 use tonic::{server::NamedService, transport::Server, Request, Response, Status};
 
@@ -27,7 +26,7 @@ async fn main() {
         .unwrap();
 
     let service = TestService {
-        canon: Arc::new(Mutex::new(canvas::blank())),
+        rooms: Arc::new(Mutex::new(Vec::new())),
     };
 
     let drawing_server = DrawingServer::new(service);
@@ -41,28 +40,80 @@ async fn main() {
 }
 
 pub struct TestService {
-    canon: Arc<Mutex<DrawingCanvas>>,
+    rooms: Arc<Mutex<Vec<DrawingCanvas>>>,
 }
 
 #[tonic::async_trait]
 impl Drawing for TestService {
-    async fn upload_canvas(
-        &self,
-        uploaded: Request<DrawingCanvas>,
-    ) -> Result<Response<Unit>, Status> {
-        let canvas = uploaded.into_inner();
 
-        let mut lock = self.canon.lock().unwrap();
+    async fn create_room(&self, request: Request<CreateRoomRequest>) -> Result<Response<CreateRoomResponse>, Status> {
 
-        canvas::merge_into(&mut *lock, &canvas);
+        let initial = if let Some(canv) = request.into_inner().initial {
+            let err = |msg| Err(Status::invalid_argument(msg));
 
-        Ok(Response::new(Unit {}))
+            use canvas::CheckInvariantsResult as C;
+            match canvas::check_invariants(&canv) {
+                C::Fine => canv,
+                C::NegativeWidth => return err(format!("Given canvas width cannot be negative! Value given: {0}.", canv.width)),
+                C::NegativeHeight => return err(format!("Given canvas height cannot be negative! Value given: {0}.", canv.height)),
+                C::OverflowingSize => return err(format!("Given canvas dimensions would cause a 32-bit integer overflow! Given canvas dimensions width {0} and height {1}, when multiplied, exceed the representation limit of 32 bits.", canv.width, canv.height)),
+                C::MismatchedSize => return err(format!("Given canvas dimensions do not match the actual size of the given contents! Given canvas dimensions width {0} and height {1}, when multiplied, do not match the number of pixels in the canvas contents ({2}).", canv.width, canv.height, canv.contents.len()))
+            }
+        } else {
+            canvas::blank(50, 50)
+        };
+
+        let mut lock = self.rooms.lock().unwrap();
+        lock.push(initial);
+        Ok(Response::new(CreateRoomResponse { room_id: (lock.len() - 1) as i32 }))
     }
 
-    async fn pull_canvas(&self, _: Request<Unit>) -> Result<Response<DrawingCanvas>, Status> {
-        let lock = self.canon.lock().unwrap();
+    async fn query_rooms(&self, _request: Request<QueryRoomsRequest>) -> Result<Response<QueryRoomsResponse>, Status> {
+        let lock = self.rooms.lock().unwrap();
+        let result: Vec<_> = lock.iter().enumerate().map(|(i, c)| RoomDetails { id: i as i32, width: c.width, height: c.height }).collect();
 
-        Ok(Response::new(lock.clone()))
+        Ok(Response::new(QueryRoomsResponse { rooms: result }))
+    }
+
+    async fn upload_canvas(
+        &self,
+        uploaded: Request<UploadCanvasRequest>
+    ) -> Result<Response<UploadCanvasResponse>, Status> {
+
+        let UploadCanvasRequest { canvas: Some(canvas), room } = uploaded.into_inner() else {
+            let msg = format!("A canvas must be included with the request!");
+            return Err(Status::invalid_argument(msg));
+        };
+
+        let mut lock = self.rooms.lock().unwrap();
+
+        let Some(target_room) = lock.get_mut(room as usize) else {
+            let msg = format!("There is no room numbered {room}!");
+            return Err(Status::out_of_range(msg));
+        };
+
+        canvas::try_merge_into(target_room, &canvas).map_err(|e| Status::invalid_argument(e))?;
+
+        Ok(Response::new(UploadCanvasResponse {}))
+    }
+
+    async fn pull_canvas(&self, request: Request<PullCanvasRequest>) -> Result<Response<DrawingCanvas>, Status> {
+        let room = request.into_inner().room;
+
+        let mut lock = self.rooms.lock().unwrap();
+
+        if lock.len() == 0 && room == 0 {
+            let initial = canvas::blank(50, 50);
+            lock.push(initial.clone());
+            return Ok(Response::new(initial));
+        }
+
+        let Some(target_room) = lock.get_mut(room as usize) else {
+            let msg = format!("There is no room numbered {room}!");
+            return Err(Status::out_of_range(msg));
+        };
+
+        Ok(Response::new(target_room.clone()))
     }
 
     async fn health_check(
@@ -83,7 +134,12 @@ impl NamedService for TestService {
 impl Clone for TestService {
     fn clone(&self) -> Self {
         Self {
-            canon: Arc::clone(&self.canon),
+            rooms: Arc::clone(&self.rooms),
         }
     }
+}
+
+#[allow(dead_code)]
+fn get_room_or_status(rooms: &mut [DrawingCanvas], idx: usize) -> Result<&mut DrawingCanvas, String> {
+    rooms.get_mut(idx).ok_or_else(|| format!("No such room: {idx}! Send a query to get information about available rooms."))
 }
